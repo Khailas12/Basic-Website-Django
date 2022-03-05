3a9026b772e35f54656a0807291dcc1c9c3c0cca
Khailas R <khailas303@gmail.com>
2022-03-06 00:10:51 +0530

    fetch and create

--- mysite/models.py
+++ mysite/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=123)
+    author = models.TextField(max_length=33)
     publish_date = models.DateTimeField('Date Published')
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .forms import BookForm
+from .models import Book
 
 
 def index(request):
-    return render(request, 'index.html')
+    book = Book.objects.all()    
+    context = {'book': book}
+    return render(request, 'index.html', context)
+
+
+def create(request):
+    form = BookForm(request.POST or None)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            form = BookForm()
+            return redirect('/')
+    
+        else:
+            Http404
+            
+    context = {'form': form}
+    return render(request, 'create.html', context)
